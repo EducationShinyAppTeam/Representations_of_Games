7068959f4aef346c7def7c4026a6d746969ad6fa
Luqi Jiao Emanuele <lzz5263@psu.edu>
2023-10-08 22:04:59 -0400

    first draft

--- app.R
+++ app.R
@@ -5,22 +5,23 @@ library(shinyBS)
 library(shinyWidgets)
 library(boastUtils)
 
-# Load additional dependencies and setup functions
-# source("global.R")
+# Define global constants and load question banks ----
+questionBank1 <- read.csv("questionBank1.csv", header = TRUE)
+questionBank1 <- na.omit(questionBank1)
 
 # Define UI for App ----
 ui <- list(
   ## Create the app page ----
   dashboardPage(
-    skin = "blue",
+    skin = "black",
     ### Create the app header ----
     dashboardHeader(
-      title = "App Template", # You may use a shortened form of the title here
+      title = "Representations", 
       titleWidth = 250,
       tags$li(class = "dropdown", actionLink("info", icon("info"))),
       tags$li(
         class = "dropdown",
-        boastUtils::surveyLink(name = "App_Template")
+        boastUtils::surveyLink(name = "Repersentations_of_Games")
       ),
       tags$li(
         class = "dropdown",
@@ -36,11 +37,9 @@ ui <- list(
         id = "pages",
         menuItem("Overview", tabName = "overview", icon = icon("gauge-high")),
         menuItem("Prerequisites", tabName = "prerequisites", icon = icon("book")),
-        menuItem("Example", tabName = "example", icon = icon("book-open-reader")),
+        menuItem("Examples", tabName = "examples", icon = icon("book-open-reader")),
         menuItem("Explore", tabName = "explore", icon = icon("wpexplorer")),
         menuItem("Challenge", tabName = "challenge", icon = icon("gears")),
-        menuItem("Game", tabName = "game", icon = icon("gamepad")),
-        menuItem("Wizard", tabName = "wizard", icon = icon("hat-wizard")),
         menuItem("References", tabName = "references", icon = icon("leanpub"))
       ),
       tags$div(
@@ -55,24 +54,25 @@ ui <- list(
         tabItem(
           tabName = "overview",
           withMathJax(),
-          h1("Sample Application for BOAST Apps"), # This should be the full name.
-          p("This is a sample Shiny application for BOAST. Remember, this page
-            will act like the front page (home page) of your app. Thus you will
-            want to have this page catch attention and describe (in general terms)
-            what the user can do in the rest of the app."),
+          h1("Repersentations of Games"),
+          p("This is an app that plays extensive form and normal form representations
+            of the games to explore how decision markers such as firms or players
+            will choose the strategies in a conflict situaion and react to the strategy 
+            of their rivals."),
           h2("Instructions"),
-          p("This information will change depending on what you want to do."),
           tags$ol(
-            tags$li("Review any prerequiste ideas using the Prerequistes tab."),
-            tags$li("Explore the Exploration Tab."),
-            tags$li("Challenge yourself."),
+            tags$li("Review key topics of components and consumption of a game."),
+            tags$li("Explore the differences of games ins extensive form and normal 
+                    form."),
+            tags$li("Challenge yourself by play the game as one of the players to 
+                    select the best strategy for you."),
             tags$li("Play the game to test how far you've come.")
           ),
-          ##### Go Button--location will depend on your goals
+          br(),
           div(
             style = "text-align: center;",
             bsButton(
-              inputId = "go1",
+              inputId = "goToExam",
               label = "GO!",
               size = "large",
               icon = icon("bolt"),
@@ -84,19 +84,15 @@ ui <- list(
           br(),
           h2("Acknowledgements"),
           p(
-            "This version of the app was developed and coded by Neil J.
-            Hatfield  and Robert P. Carey, III.",
-            br(),
-            "We would like to extend a special thanks to the Shiny Program
-            Students.",
+            "This version of the app was developed and coded by Luqi Jiao Emanuele.",
             br(),
             br(),
             "Cite this app as:",
             br(),
-            citeApp(),
+            boastUtils::citeApp(),
             br(),
             br(),
-            div(class = "updated", "Last Update: 11/8/2022 by NJH.")
+            div(class = "updated", "Last Update: 7/11/2023 by LJE.")
           )
         ),
         #### Set up the Prerequisites Page ----
@@ -104,67 +100,150 @@ ui <- list(
           tabName = "prerequisites",
           withMathJax(),
           h2("Prerequisites"),
-          p("In order to get the most out of this app, please review the
-            following:"),
+          p("Three elements of a game:"),
           tags$ul(
-            tags$li("Pre-req 1--Technical/Conceptual Prerequisites are ideas that
-                    users need to have in order to engage with your app fully."),
-            tags$li("Pre-req 2--Contextual Prerequisites refer to any information
-                    about a context in your app that will enrich a user's
-                    understandings."),
-            tags$li("Pre-req 3"),
-            tags$li("Pre-req 4")
+            tags$li("A set of players."),
+            tags$li("A set of strategies for each player."),
+            tags$li("A payoff function with a payoff to each player with all possible 
+                    strategies.")
           ),
-          p("Notice the use of an unordered list; users can move through the
-            list any way they wish."),
-          box(
-            title = strong("Null Hypothesis Significance Tests (NHSTs)"),
-            status = "primary",
-            collapsible = TRUE,
-            collapsed = TRUE,
-            width = '100%',
-            "In the Confirmatory Data Analysis tradition, null hypothesis
-            significance tests serve as a critical tool to confirm that a
-            particular theoretical model describes our data and to make a
-            generalization from our sample to the broader population
-            (i.e., make an inference). The null hypothesis often reflects the
-            simpler of two models (e.g., 'no statistical difference',
-            'there is an additive difference of 1', etc.) that we will use to
-            build a sampling distribution for our chosen estimator. These
-            methods let us test whether our sample data are consistent with this
-            simple model (null hypothesis)."
+          br(),
+          p("Assumptions of payoff matrix:"),
+          tags$ul(
+            tags$li("The players will play their strategies simultaneously."),
+            tags$li("This is one-shot game, meaning all players paly only once.")
           ),
           box(
-            title = strong(tags$em("p"), "-values"),
+            title = strong("Extensive Form"),
             status = "primary",
             collapsible = TRUE,
             collapsed = FALSE,
             width = '100%',
-            "The probability that our selected estimator takes on a value at
-            least as extreme as what we observed given our null hypothesis. If
-            we were to carry out our study infinitely many times and the null
-            hypothesis accurately modeled what we're studying, then we would
-            expect for our estimator to produce a value at least as extreme as
-            what we have seen 100*(p-value)% of the time. The larger the
-            p-value, the more often we would expect our estimator to take on a
-            value at least as extreme as what we've seen; the smaller, the less
-            often."
+            "We will use game tree in extensive form.",
+            tags$figure(
+              class = "centerFigure",
+              tags$img(
+                scr = "www/prere1.jpg",
+                alt = "Example of extensive form in game tree."
+                )
+              ),
+            br(),
+            "There are three sets of objects in extensive form:",
+            tags$ol(
+              tags$li("Nodes"),
+              tags$li("Branches"),
+              tags$li("Information sets")
+              )
+            ),
+          box(
+            title = strong("Normal Form"),
+            status = "primary",
+            collapsible = TRUE,
+            collapsed = FALSE,
+            width = '100%',
+            "We will use payoff matrix in normal form.",
+            tags$figure(
+              class = "centerFigure",
+              tags$img(
+                scr = "www/prere2.jpg",
+                alt = "Example of normal form in payoff matrix."
+                )
+              ),
+            br(),
+            "There are three sets of objects in normal form:",
+            tags$ol(
+              tags$li("Players"),
+              tags$li("Strategies"),
+              tags$li("Payoffs")
+              )
+            )
+          ),
+        #### Set up the Examples Page ----
+        tabItem(
+          tabName = "examples",
+          fluidPage(
+            tabsetPanel(
+              #####First tab ----
+              tabPanel(
+                title = "Extensive Form",
+                br(),
+                h3("Game Tree"),
+                br(),
+                p("Test your understanding by trying out these questions."),
+                fluidRow(
+                  uiOutput(outputId = "questionPlot"),
+                  wellPanel(
+                  br(),
+                  selectInput(
+                    inputId = "response",
+                    label = "Select your answer", 
+                    choices = list("A", "B", "C", ""), 
+                    selected = ""
+                  ),
+                  bsButton(
+                    inputId = "submit",
+                    label = "Submit",
+                    size = "large",
+                    style = "default"
+                  ),
+                  br(),
+                  br(),
+                  p("Feedback"),
+                  uiOutput("icon"),
+                  uiOutput("answer"),
+                  bsButton(
+                    inputId = "newChallenge",
+                    label = "New Challenge",
+                    size = "large",
+                    style = "default"
+                  )
+                  )
+                )
+              ),
+              #####Second tab ----
+              tabPanel(
+                title = "Normal Form",
+                br(),
+                h3("Payoff Matrix"),
+                br(),
+                p(""),
+                
+              ),
+            )
           )
+          
         ),
-        #### Note: you must have at least one of the following pages. You might
-        #### have more than one type and/or more than one of the same type. This
-        #### will be up to you and the goals for your app.
         #### Set up an Explore Page ----
         tabItem(
           tabName = "explore",
-          withMathJax(),
-          h2("Explore the Concept"),
-          p("This page should include something for the user to do, the more
-            active and engaging, the better. The purpose of this page is to help
-            the user build a productive understanding of the concept your app
-            is dedicated to."),
-          p("Common elements include graphs, sliders, buttons, etc."),
-          p("The following comes from the NHST Caveats App:"),
+          fluidPage(
+            tabsetPanel(
+              #####First tab ----
+              tabPanel(
+                title = "Extensive Form",
+                br(),
+                h3("Game Tree"),
+                br(),
+                p("")
+              ),
+              #####Second tab ----
+              tabPanel(
+                title = "Normal Form",
+                br(),
+                h3("Payoff Matrix"),
+              ),
+            )
+          ),
+          div(
+            style = "text-align: center;",
+            bsButton(
+              inputId = "goToChallenge",
+              label = "Challenge!",
+              size = "large",
+              icon = icon("bolt"),
+              style = "default"
+            )
+          ),
         ),
         #### Set up a Challenge Page ----
         tabItem(
@@ -179,7 +258,7 @@ ui <- list(
             consider is to re-create the tools of the Exploration page and then
             a list of questions for the user to then answer.")
         ),
-        #### Set up a Game Page ----
+        #### Set up a Simulation Page ----
         tabItem(
           tabName = "game",
           withMathJax(),
@@ -188,29 +267,18 @@ ui <- list(
             Game types include Tic-Tac-Toe, Matching, and a version Hangman to
             name a few. If you have ideas for new game type, please let us know.")
         ),
-        #### Set up a Wizard Page ----
-        tabItem(
-          tabName = "wizard",
-          withMathJax(),
-          h2("Wizard"),
-          p("This page will have a series of inputs and questions for the user to
-            answer/work through in order to have the app create something. These
-            types of Activity pages are currently rare as we try to avoid
-            creating 'calculators' in the BOAST project.")
-        ),
         #### Set up the References Page ----
         tabItem(
           tabName = "references",
           withMathJax(),
           h2("References"),
-          p("You'll need to fill in this page with all of the appropriate
-            references for your app."),
           p(
             class = "hangingindent",
             "Bailey, E. (2015). shinyBS: Twitter bootstrap components for shiny.
             (v0.61). [R package]. Available from
             https://CRAN.R-project.org/package=shinyBS"
           ),
+          p("https://www.investopedia.com/terms/m/matching-pennies.asp#:~:text=Matching%20Pennies%20is%20a%20basic%20game%20theory%20example,to%20the%20players%20that%20are%20not%20the%20same."),
           br(),
           br(),
           br(),
@@ -236,7 +304,171 @@ server <- function(input, output, session) {
       )
     }
   )
-
+  
+  ## Move to Exam page ----
+  observeEvent(
+    eventExpr = input$goToExam,
+    handlerExpr = {
+      updateTabItems(
+        session = session,
+        inputId = "pages",
+        selected = "examples"
+      )
+    }
+  )
+  
+  ## Examples Page Buttons ----
+  ### Payoff Matrix ----
+  challengeElements <- reactiveValues(
+    # Creates a vector of shuffled integers which we can use for the id column
+    promptIds = sample(1:nrow(questionBank), size = nrow(questionBank), replace = FALSE),
+    # Create current index
+    currentIndex = 1,
+    # Create a flag if current question has been answered
+    answered = FALSE,
+    # Create a flag for first visit to page
+    firstTime = TRUE
+  )
+  
+  ### Set watcher to iterate current question
+  observeEvent(
+    eventExpr = c(input$pages, input$newChallenge),
+    handlerExpr = {
+      if (input$pages == "challenge" & challengeElements$firstTime) {
+        challengeElements$firstTime <- FALSE
+      } else if (challengeElements$currentIndex == nrow(questionBank1)) {
+        sendSweetAlert(
+          session = session,
+          type = "error",
+          title = "End of Game",
+          text = "You've played through all of the questions."
+        )
+      } else {
+        challengeElements$currentIndex <- challengeElements$currentIndex + 1
+      }
+    }
+  )
+  
+  ### Display challenge plot and question ----
+  scoreLevel <- reactiveVal(0)
+  
+  output$questionPlot <- renderUI(
+    expr = {
+      questionId <- challengeElements$promptIds[challengeElements$currentIndex]
+      tagList(
+        h3("Question Plot"),
+        HTML(questionBank1$extraOutput[questionId]),
+        p(questionBank1$question[questionId])
+      )
+    }
+  )
+  
+  random_order <- reactiveVal()
+  
+  random_choice <- function() {
+    choices <- c("1", "2", "3")
+    random_order <- sample(choices)
+    return(random_order)
+  }
+  
+  random_choice()
+  
+  observe({
+    random_order_val <- random_order()
+    
+    output$choiceA <- renderUI({
+      questionId <- challengeElements$promptIds[challengeElements$currentIndex]
+      tagList(
+        HTML(questionBank1[[paste0("A")]][questionId])
+      )
+    })
+    
+    output$choiceB <- renderUI({
+      questionId <- challengeElements$promptIds[challengeElements$currentIndex]
+      tagList(
+        HTML(questionBank1[[paste0("B")]][questionId])
+      )
+    })
+    
+    output$choiceC <- renderUI({
+      questionId <- challengeElements$promptIds[challengeElements$currentIndex]
+      tagList(
+        HTML(questionBank1[[paste0("C")]][questionId])
+      )
+    })
+  })
+  
+  
+  
+  current_question <- reactiveVal()
+  
+  random_question <- function() {
+    random_index <- sample(nrow(questionBank1), 1)
+    current_question(questionBank1[random_index, ])
+  }
+  
+  random_question()
+  
+  output$question <- renderText({
+    current_question()$question
+  })
+  
+  output$extraOutput <- renderImage({
+    if (!is.null(current_question()$extraOutput) && nchar(current_question()$extraOutput) > 0) 
+    {
+      return(list(src = current_question()$extraOutput))
+    }
+  },
+  deleteFile = FALSE)
+  
+  output$A <- renderText({
+    if (!is.null(current_question()$A) && nchar(current_question()$A) > 0) 
+    {
+      return(list(src = current_question()$A))
+    }
+  })
+  
+  output$B <- renderText({
+    if (!is.null(current_question()$B) && nchar(current_question()$B) > 0) 
+    {
+      return(list(src = current_question()$B))
+    }
+  })
+  
+  output$C <- renderText({
+    if (!is.null(current_question()$C) && nchar(current_question()$C) > 0) 
+    {
+      return(list(src = current_question()$C))
+    }
+  })
+  
+  observeEvent(
+    eventExpr = input$submit, 
+    handlerExpr = {
+      user_answer <- input$response
+      correct_answer <- current_question()$answer
+    }
+  )
+  
+  ### Get new challenge and reset feedback ----
+  observeEvent(
+    eventExpr = input$newChallenge,
+    handlerExpr = {
+      random_choice()
+      random_question()
+      scoreLevel()
+      
+      updateSelectInput(
+        session = session,
+        inputId = "answer",
+        selected = ""
+      )
+      
+      output$icon <- renderIcon()
+      output$response <- renderUI(NULL)
+      
+    }
+  )
 
 }
 
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# App Title
+# Representations of Games
 
 [![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/) 
 ![experimental](https://img.shields.io/badge/lifecycle-experimental-orange)
@@ -8,4 +8,4 @@
 ![App Screenshot](../docs/screenshot.png)
 
 # App Description
-Type the description of your app here
+This app introduces the concept of two different forms of game theory in Economics.
